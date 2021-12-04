749b058b9bb1b33cac418002a90b490ea627decf
kmvdj23 <kristophermatthewdejesus@gmail.com>
2021-12-04 20:55:05 +0800

    CORS expose headers

--- app/config/config.py
+++ app/config/config.py
@@ -11,6 +11,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../../database.db'
 app.config['JWT_ACCESS_LIFESPAN'] = {'hours': 24}
 app.config['JWT_REFRESH_LIFESPAN'] = {'days': 30}
+app.config['CORS_EXPOSE_HEADERS'] = [ "Content-Type", "X-CSRFToken", 'content-type', 'x-csrftoken' ]
 app.config['DEBUG'] = True
 
 db = SQLAlchemy(app)
