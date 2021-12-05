8d6e428434b8e156ec8849f810dda1a134e5751b
kmvdj23 <kristophermatthewdejesus@gmail.com>
2021-12-05 19:59:30 +0800

    Add complete swagger API

--- app/config/config.py
+++ app/config/config.py
@@ -13,10 +13,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../../database.db'
 app.config['JWT_ACCESS_LIFESPAN'] = {'hours': 24}
 app.config['JWT_REFRESH_LIFESPAN'] = {'days': 30}
 app.config['CORS_EXPOSE_HEADERS'] = [ "Content-Type", "X-CSRFToken", 'content-type', 'x-csrftoken' ]
+app.config['SWAGGER_SUPPORTED_SUBMIT_METHODS'] = []
 app.config['DEBUG'] = True
 
+
 db = SQLAlchemy(app)
-api = Api(app)
+api = Api(app, title='Simple Twitter Flask API')
 guard = Praetorian()
 cors = CORS(app, supports_credentials=True)
 ma = Marshmallow()
--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,5 +1,4 @@
 from flask_restx import apidoc, Resource, reqparse
-import json
 from app.config import app, api
 from app.controllers.api import v1
 
@@ -11,10 +10,8 @@ app.register_blueprint(v1)
 @api.doc(params={ 'username' : 'Username', 'password': 'Password'})
 class AccountLogin(Resource):
     def post(self, username, password):
-        return json.dumps({
-            "username" : username,
-            "password" : password
-        })
+        return {}
+
 
 @api.route('/api/v1/signup')
 @api.doc(params={ 'username': 'Username', 'email': 'Email', 'password': 'Password'})
@@ -22,10 +19,72 @@ class AccountSignup(Resource):
     def post(self, username, email, password):
         return {}
 
+
 @api.route('/api/v1/tweet')
 @api.doc(params={ 'text': 'Tweet text'})
 class AccountTweet(Resource):
+    @api.doc(security={'Authorization' : 'auth_token'})
     def post(self):
+        return {}
+
+
+@api.route('/api/v1/logout')
+class AccountLogout(Resource):
+    @api.doc(security={'Authorization' : 'auth_token'})
+    def post(self):
+        return {}
+
+
+@api.route('/api/v1/account')
+class Profile(Resource):
+    @api.doc(security={'Authorization' : 'auth_token'})
+    def get(self):
+        return {}
+
+
+@api.route('/api/v1/tweets')
+class ProfileTweets(Resource):
+    @api.doc(security={'Authorization' : 'auth_token'})
+    def get(self):
+        return {}
+
+
+@api.route('/api/v1/account/<username>')
+class Account(Resource):
+    @api.doc(security={'Authorization' : 'auth_token'})
+    def get(self):
+        return {}
+
+
+@api.route('/api/v1/account/<username>/tweets')
+class AccountTweets(Resource):
+    @api.doc(security={'Authorization' : 'auth_token'})
+    def get(self):
+        return {}
+
+
+@api.route('/api/v1/users')
+class Users(Resource):
+    @api.doc(security={'Authorization' : 'auth_token'})
+    def get(self):
+        return {}
+
+
+@api.route('/api/v1/update_password')
+class PasswordUpdate(Resource):
+    @api.doc(security={'Authorization' : 'auth_token'})
+    def post(self):
+        return {}
+
+    @api.doc(security={'Authorization' : 'auth_token'})
+    def put(self):
+        return {}
+
+
+@api.route('/api/v1/delete_tweet/<tweet_id>')
+class DeleteTweet(Resource):
+    @api.doc(security={'Authorization' : 'auth_token'})
+    def delete(self):
         return {}
 
 
